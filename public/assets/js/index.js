af30ef25d4fc9acdd3d68d2e3870f720cc485430
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-06-18 23:42:17 -0500

    js for appending start button and removing button when clicked

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,6 +2,26 @@
 const callback = () => {
   // Handler when the DOM is fully loaded
 
+  // Attached Start Quiz Button
+  const qDiv = document.querySelector(".quiz-div");
+
+  const startBtn = document.createElement("button");
+
+  startBtn.setAttribute("type", "button");
+  startBtn.setAttribute("class", "btn btn-outline-primary align-self-center start-btn");
+
+  const text = document.createTextNode("Start New Quiz!");
+
+  startBtn.appendChild(text);
+  qDiv.appendChild(startBtn);
+
+  
+  document.querySelector(".start-btn").addEventListener("click", function(){
+    qDiv.removeChild(startBtn);
+
+    
+
+  });
 
 
 
@@ -11,12 +31,6 @@ const callback = () => {
 
 
 
-
-
-
-
-
-  console.log("hello")
 };
 
 if (
